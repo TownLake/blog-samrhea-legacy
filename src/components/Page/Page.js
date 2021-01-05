b05d5a96110f3428b3b7b3e83a8cc94b0a3d7e15
TownLake <srhea@cloudflare.com>
2021-01-05 18:21:39 +0000

    breakpoint-sm

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import styles from './Page.module.scss';
 
 type Props = {
@@ -7,14 +7,9 @@ type Props = {
 };
 
 const Page = ({ title, children }: Props) => {
-  const pageRef = useRef();
-
-  useEffect(() => {
-    pageRef.current.scrollIntoView();
-  });
 
   return (
-    <div ref={pageRef} className={styles['page']}>
+    <div className={styles['page']}>
       <div className={styles['page__inner']}>
         { title && <h1 className={styles['page__title']}>{title}</h1>}
         <div className={styles['page__body']}>
