81037f164261a18c3b73ae103f1a87a239b8bb3f
Samuel Rhea <samuelrhea@gmail.com>
2022-12-18 20:33:28 +0000

    Update post-template.js

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -40,9 +40,7 @@ export const query = graphql`
         description
         tags
         title
-        twitterImage
-        socialImage {
-          publicURL
+        socialImage
         }
       }
     }
