707b09ce2b548ee1b7e6672d30bc79554e3fad8c
Samuel Rhea <samuelrhea@gmail.com>
2022-12-19 19:34:07 +0000

    remove read link

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -45,12 +45,6 @@ const Feed: React.FC<Props> = ({ edges }: Props) => (
         <p className={styles.description}>
           {edge.node.frontmatter.description}
         </p>
-        <Link
-          className={styles.more}
-          to={edge.node.frontmatter?.slug || edge.node.fields.slug}
-        >
-          Read
-        </Link>
       </div>
     ))}
   </div>
